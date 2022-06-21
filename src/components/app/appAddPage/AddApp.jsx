045c44d5ec5acc6234359ad3a16a6fa81f300dd0
dineshnikhil <dineshkumarpokkula@gmail.com>
2022-06-21 13:19:16 +0530

    added useref to addapp page

--- src/components/app/appAddPage/AddApp.jsx
+++ src/components/app/appAddPage/AddApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../../ui/input/Input'
 import LogBtn from '../../ui/logBtn/LogBtn'
@@ -7,6 +8,11 @@ import classes from './AddApp.module.css'
 import card2 from '../../../images/card2.svg'
 
 function AddApp() {
+
+    const appName = useRef();
+    const email = useRef();
+    const password = useRef();
+
   return (
     <div className={classes.signupDiv}>
         <div className={classes.signupImgDiv}>
@@ -16,9 +22,9 @@ function AddApp() {
             <form>
                 <h1>Add Your App</h1>
                 <p>we ensure that your data is safe</p>
-                <Input type="text" label="AppName" />
-                <Input type="email" label="Email" />
-                <Input type="password" label="Password" />
+                <Input type="text" label="AppName" onRef={appName} />
+                <Input type="email" label="Email" onRef={email} />
+                <Input type="password" label="Password" onRef={password} />
                 <LogBtn log="Add"/>
                 <div>
                     Don't want to Add? <Link to='/' className={classes.forgotLink}>Go Back</Link>
